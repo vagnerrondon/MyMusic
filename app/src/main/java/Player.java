d030262496ad7ea2ccf53788b9b9187953a0c2de
root <root@rondonlinux.casa>
2016-06-28 07:30:05 -0300

    Initial commit

---
+++ app/src/main/java/Player.java
@@ -0,0 +1,22 @@
+/**
+ * Created by root on 26/06/16.
+ */
+public class Player {
+    public boolean Play()
+    {
+        return true;
+    };
+        public boolean Stop()
+        {
+            return true;
+        };
+    public boolean Foward()
+    {
+        return true;
+    };
+    public boolean Backward()
+    {
+        return true;
+    };
+
+}
